04c49721ddd9172d7c2616a41c7ddae4673f0f8a
Francisco Picado <franciscopicado19@gmail.com>
2021-12-15 18:27:37 +0100

    📝  design login

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,8 @@ Encore
    // .addEntry('app', './assets/app.js')
    .addEntry('app', './assets/js/app.js')
    .addEntry('admin', './assets/js/admin.js')
+   .addEntry('login', './assets/js/login.js')
+
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
   /*   .enableStimulusBridge('./assets/controllers.json') */
